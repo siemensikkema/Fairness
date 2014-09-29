4f8f336b77af4f2679c8907226713ba5dd8b18f3
Siemen Sikkema <s.h.sikkema@gmail.com>
2014-09-29 20:22:12 +0200

    Use compressional resistance to make autolayout perform sizing automatically depending on balance intrinsic size

--- Fairness/ParticipantCell.swift
+++ Fairness/ParticipantCell.swift
@@ -5,13 +5,11 @@ class ParticipantCell: UITableViewCell {
 
     @IBOutlet weak var balanceLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var balanceLabelWidthConstraint: NSLayoutConstraint!
 
 
     func configure(participantViewModel: ParticipantViewModel) {
 
         nameLabel.text = participantViewModel.name
         balanceLabel.text = participantViewModel.balanceString
-        balanceLabelWidthConstraint.constant = balanceLabel.intrinsicContentSize().width
     }
 }
