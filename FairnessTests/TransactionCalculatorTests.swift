77e1074eabc616c78c6fb1674d378ec5546737c6
Siemen Sikkema <s.h.sikkema@gmail.com>
2014-11-22 15:46:46 +0100

    Add more TransactionCalculator tests

--- FairnessTests/TransactionCalculatorTests.swift
+++ FairnessTests/TransactionCalculatorTests.swift
@@ -3,11 +3,16 @@ import XCTest
 class TransactionCalculatorTests: XCTestCase {
 
     var sut: TransactionCalculator!
-    let participantStore = ParticipantStore(participants: [Participant(name: "name1"), Participant(name: "name2")])
+    var participantStore: ParticipantStore!
 
     override func setUp() {
 
+        participantStore = ParticipantStore(participants: [Participant(name: "name1"), Participant(name: "name2")])
         sut = TransactionCalculator(modelDidBecomeInvalidCallback: { participantTransactionModels in }, participantStore: participantStore)
+
+        sut.togglePayerAtIndex(0)
+        sut.togglePayeeAtIndex(1)
+        sut.cost = 1.23
     }
 }
 
@@ -18,21 +23,61 @@ extension TransactionCalculatorTests {
         XCTAssertEqual(sut.participantTransactionModels.count, 2)
     }
 
-    func testHasPayerReturnsFalseWhenNoParticipantsArePayer() {
-
-        XCTAssertFalse(sut.hasPayer)
-    }
-
     func testHasPayerReturnsTrueWhenOneParticipantsIsPayer() {
 
-        sut.togglePayerAtIndex(0)
         XCTAssertTrue(sut.hasPayer)
     }
+    
+    func testHasPayerReturnsFalseWhenNoParticipantsArePayer() {
 
-    func testTogglePayerTwiceSetsHasPayerStatusToFalse() {
-
-        sut.togglePayerAtIndex(0)
         sut.togglePayerAtIndex(0)
         XCTAssertFalse(sut.hasPayer)
+    }
+
+    func testTransactionWithPayerAndPayeeAndNonZeroCostIsValid() {
+
+        XCTAssertTrue(sut.isValid)
+    }
+
+    func testTransactionIsInvalidWithoutPayer() {
+
+        sut.togglePayerAtIndex(0)
+        XCTAssertFalse(sut.isValid)
+    }
+
+    func testTransactionIsInvalidWithoutPayee() {
+
+        sut.togglePayeeAtIndex(1)
+        XCTAssertFalse(sut.isValid)
+    }
+
+    func testTransactionIsInvalidWithZeroCost() {
+
+        sut.cost = 0
+        XCTAssertFalse(sut.isValid)
+    }
+
+    func testTransactionIsInvalidWhenPayerIsTheOnlyPayee() {
+
+        sut.togglePayeeAtIndex(0)
+        sut.togglePayeeAtIndex(1)
+        XCTAssertFalse(sut.isValid)
+    }
+
+    func testTransactionIsValidWhenPayerIsOneOfThePayees() {
+
+        sut.togglePayeeAtIndex(0)
+        XCTAssertTrue(sut.isValid)
+    }
+
+    func testTransactionAmountsForSimpleTransactionAreCorrect() {
+
+        XCTAssertEqual(sut.participantTransactionModels.map { $0.maybeAmount! }, [1.23, -1.23])
+    }
+
+    func testTransactionAmountsForSharedTransactionAreCorrect() {
+
+        sut.togglePayeeAtIndex(0)
+        XCTAssertEqual(sut.participantTransactionModels.map { $0.maybeAmount! }, [0.615, -0.615])
     }
 }
