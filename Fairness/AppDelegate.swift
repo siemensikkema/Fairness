d780922000eae240f6262512aefc8e4baf8f11c1
Siemen Sikkema <s.h.sikkema@gmail.com>
2014-11-23 12:27:58 +0100

    Add function to differentiate between test run and normal run of the app

--- Fairness/AppDelegate.swift
+++ Fairness/AppDelegate.swift
@@ -1,7 +1,18 @@
 import UIKit
 
+func isRunningTests() -> Bool {
+
+    return NSProcessInfo.processInfo().environment["XCInjectBundle"] != nil
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+
+        println("is running tests? \(isRunningTests())")
+        return true
+    }
 }
